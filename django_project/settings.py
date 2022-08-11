be3533174fd732fde8110091a9d3cdcf162ca74b
mohamed hesham <hesmammohammed@gmail.com>
2022-08-11 10:43:26 +0200

    Django REST Framework

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    "rest_framework",
+    # local
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
 ]
 
 MIDDLEWARE = [
