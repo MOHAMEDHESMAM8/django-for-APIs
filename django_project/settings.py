666845282ad12a131d9163a767bce16d6a0b0886
mohamed hesham <hesmammohammed@gmail.com>
2022-08-29 14:10:35 +0200

    static

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"] # new
 STATIC_ROOT = BASE_DIR / "staticfiles" # new
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
